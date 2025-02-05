dd4bcca0f34ef569e3f7280f84b7be8dc3a55dd5
Savita Rao <savitarao702@gmail.com>
2025-02-05 12:00:49 +0530

    some changes in css

---
+++ index.js
@@ -0,0 +1,10 @@
+// const http = require('node:http');
+// const server = http.createServer((req, res) => {
+//   console.log(req.method);
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.end("<h2>This is a computer<h2>");
+// });
+
+// server.listen(3031);
+
+
